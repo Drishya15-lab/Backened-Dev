0219d215c5331cbf421d5c8dddadfb059452e92b
Drishya Singh <drishya.singh_cs23@gla.ac.in>
2026-01-19 14:51:57 +0530

    Add files via upload

---
+++ eventloop.js
@@ -0,0 +1,4 @@
+setTimeout(() => console.log('setTimeout'), 0);
+setImmediate(() => console.log('setImmediate'));
+process.nextTick(() => console.log('nextTick'));
+Promise.resolve().then(() => console.log('Promise'));
